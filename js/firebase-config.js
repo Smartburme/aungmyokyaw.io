3235756685eb2c4aab88d619fd88b9985082cf3a
smartburme <athuyrain@gmail.com>
2025-06-08 14:28:47 +0900

    Create firebase-config.js
    Signed-off-by: smartburme <athuyrain@gmail.com>

---
+++ js/firebase-config.js
@@ -0,0 +1,6 @@
+// Firebase configuration exported for use in other files
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "your-project.firebaseapp.com",
+    // ... other config
+};
