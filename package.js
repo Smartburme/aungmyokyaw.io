8a2525c6f061ff183459445a05b88ac363c9ecc4
smartburme <athuyrain@gmail.com>
2025-06-13 02:21:58 +0630

    Create package.js
    Signed-off-by: smartburme <athuyrain@gmail.com>

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "smart-burme-ai",
+  "version": "1.0.0",
+  "description": "Myanmar AI Chatbot",
+  "scripts": {
+    "start": "node server.js"  // (သို့) live-server သုံးပါက "live-server ."
+  },
+  "dependencies": {
+    "express": "^4.18.2"  // (လိုအပ်ပါက)
+  }
+}
