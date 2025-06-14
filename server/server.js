0303830867fc9289995dd7d12d13a848028f9094
smartburme <athuyrain@gmail.com>
2025-06-14 08:06:02 +0630

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { OpenAI } = require('openai');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
